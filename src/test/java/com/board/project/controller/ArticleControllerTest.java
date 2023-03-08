aebcc8a1e857555c10266ba184d7cef4059b00c6
jogony <seonwongim@gmail.com>
2023-03-09 03:42:49 +0900

    #9 - 게시판 뷰 기본 골격 작성 컨트롤러 테스트를 통과하게 열어줌 뷰 적절히 뼈대 잡음 테스트에서는 media type 검사에서 부가적인 옵션이 들어갈 수 있음을 확인했기 때문에 적절히 텍스트 메세지를 적절히 수정

--- src/test/java/com/board/project/controller/ArticleControllerTest.java
+++ src/test/java/com/board/project/controller/ArticleControllerTest.java
@@ -22,7 +22,6 @@ class ArticleControllerTest {
         this.mockMvc = mockMvc;
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -31,7 +30,7 @@ class ArticleControllerTest {
         // When & Then
         mockMvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
@@ -70,7 +69,7 @@ class ArticleControllerTest {
     void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
         // Given
 
-        // When & Then
+        // When & Then`
         mockMvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
