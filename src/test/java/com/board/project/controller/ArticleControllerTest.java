d1f614518a91a9310c2139469ad8ee8dda3152a9
jogony <seonwongim@gmail.com>
2023-03-09 07:10:22 +0900

    #9-스프링 시큐리티 디펜던시 추가 인증 구현할 때 넣으려고 했는데, 뷰를 간단하게 구상하기 위해 작성했다.

--- src/test/java/com/board/project/controller/ArticleControllerTest.java
+++ src/test/java/com/board/project/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package com.board.project.controller;
 
+import com.board.project.config.SpringSecurity;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,6 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SpringSecurity.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
@@ -35,7 +38,6 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 상세 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
